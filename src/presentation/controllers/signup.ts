99f36e8937b41327472af26a30f97c65269da9c6
Tiago Poli <tiagoluizpoli@gmail.com>
2023-10-14 13:44:36 -0300

    rafactor: ensure signUpController.handle implement correct http interfaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,19 +1,25 @@
+import { HttpRequest, HttpResponse } from '../protocols/http';
+
 export class SignUpController {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  handle = (httpRequest: any): any => {
+  handle = (httpRequest: HttpRequest): HttpResponse => {
     if (!httpRequest.body.name) {
       return {
-        httpRequest,
         statusCode: 400,
         body: new Error('Missing param: name'),
       };
     }
     if (!httpRequest.body.email) {
       return {
-        httpRequest,
         statusCode: 400,
         body: new Error('Missing param: email'),
       };
     }
+    return {
+      statusCode: 400,
+      body: {
+        message: 'success',
+      },
+    };
   };
 }
