4dc665de5434b757c3e2bbee6f3265443e59ee9c
Tiago Poli <tiagoluizpoli@gmail.com>
2023-10-14 18:56:42 -0300

    test: ensure SugnUpControler calls email validator with correct email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -97,4 +97,19 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new InvalidParamError('email'));
   });
+  test('Shoud call emailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut();
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid');
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any@main.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    sut.handle(httpRequest);
+
+    expect(isValidSpy).toHaveBeenCalledWith('any@main.com');
+  });
 });
