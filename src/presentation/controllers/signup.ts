374f8d41b02770cd9ebf841a3cab4598c3b68075
Tiago Poli <tiagoluizpoli@gmail.com>
2023-10-14 13:37:08 -0300

    feat: ensure signup controller returns an error and statusCode 400 if no name is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,19 @@
 export class SignUpController {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   handle = (httpRequest: any): any => {
-    return {
-      httpRequest,
-      statusCode: 400,
-      body: new Error('Missing param: name'),
-    };
+    if (!httpRequest.body.name) {
+      return {
+        httpRequest,
+        statusCode: 400,
+        body: new Error('Missing param: name'),
+      };
+    }
+    if (!httpRequest.body.email) {
+      return {
+        httpRequest,
+        statusCode: 400,
+        body: new Error('Missing param: email'),
+      };
+    }
   };
 }
