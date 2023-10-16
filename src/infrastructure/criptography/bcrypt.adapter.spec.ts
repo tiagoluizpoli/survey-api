7c1473f72eb1bcb0b8833c290fec8b5e00dcfe65
Tiago Poli <tiago.poli@stoneage.com.br>
2023-10-16 01:01:17 -0300

    feat: ensure BCryptAdapter returns a hash on success

--- src/infrastructure/criptography/bcrypt.adapter.spec.ts
+++ src/infrastructure/criptography/bcrypt.adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt';
 import { BcryptAdapter } from './bcrypt.adapter';
 
+jest.mock('bcrypt', () => ({
+  hash: async (): Promise<string> => {
+    return Promise.resolve('hashed_password');
+  },
+}));
+
 describe('Bcrypt Adapter', () => {
   it('should call bcrypt with correct values', async () => {
     const salt = 12;
@@ -11,5 +17,14 @@ describe('Bcrypt Adapter', () => {
     await sut.encrypt('any_value');
 
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
+  });
+
+  it('should return a hash on success', async () => {
+    const salt = 12;
+    const sut = new BcryptAdapter(salt);
+
+    const hashedValue = await sut.encrypt('any_value');
+
+    expect(hashedValue).toBe('hashed_password');
   });
 });
