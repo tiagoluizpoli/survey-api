4134469a1377a74eaf1bbf23b81e68e334815755
Tiago Poli <tiago.poli@stoneage.com.br>
2023-10-15 23:05:06 -0300

    feat: ensure DbAddAccount throws if DbAddAccountRepository throws

--- src/data/usecases/addAccont/dbAddAccount.spec.ts
+++ src/data/usecases/addAccont/dbAddAccount.spec.ts
@@ -94,4 +94,22 @@ describe('DbAddAccount Usecase', () => {
       password: 'hashed_password',
     });
   });
+
+  it('should throw if encrypter throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut();
+    jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(
+      new Promise((_, rejects) => {
+        return rejects(new Error());
+      }),
+    );
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password',
+    };
+    const promise = sut.add(accountData);
+
+    await expect(promise).rejects.toThrow();
+  });
 });
