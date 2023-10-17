729faaa98bc2e23d9b9060d86f564d1e3f3277dd
Tiago Poli <tiagoluizpoli@gmail.com>
2023-10-17 19:03:30 -0300

    feat: ensure cores is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
-import { bodyParser } from '../middlewared/bodyParser';
+import { bodyParser } from '../middlewares/bodyParser';
 import { Express } from 'express';
+import { cors } from '../middlewares/cors';
 
 export default (app: Express): void => {
   app.use(bodyParser);
+  app.use(cors);
 };
