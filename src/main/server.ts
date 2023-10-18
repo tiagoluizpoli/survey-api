cc2ebfdd4c8fe223702da0640ae68de176baf422
Tiago Poli <tiagoluizpoli@gmail.com>
2023-10-18 01:12:23 -0300

    chore: made the mongo db work

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,15 @@
-import app from './config/app';
+import { MongoHelper } from '../infrastructure';
+import { env } from './config';
 
-app.listen(5050, () =>
-  // eslint-disable-next-line no-console
-  console.log(`Server runing at http://localhost:${5050}`),
-);
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default;
+    app.listen(env.port, () =>
+      // eslint-disable-next-line no-console
+      console.log(`Server runing at http://localhost:${env.port}`),
+    );
+  })
+  .catch((err) => {
+    // eslint-disable-next-line no-console
+    console.log(err);
+  });
