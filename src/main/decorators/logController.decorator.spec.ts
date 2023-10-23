677380771a44c84e16dbcc276b27db9a875dca05
Tiago Poli <tiagoluizpoli@gmail.com>
2023-10-22 22:15:44 -0300

    feat: ensure decorator return the same result as the controller

--- src/main/decorators/logController.decorator.spec.ts
+++ src/main/decorators/logController.decorator.spec.ts
@@ -21,6 +21,7 @@ const makeController = (): Controller => {
   }
   return new ControllerStub();
 };
+
 const makeSut = (): makeSutResult => {
   const controllerStub = makeController();
   const sut = new LogControllerDecorator(controllerStub);
@@ -45,5 +46,25 @@ describe('LogController Decorator', () => {
     };
     await sut.handle(httpRequest);
     expect(handleSpy).toHaveBeenCalledWith(httpRequest);
+  });
+
+  it('should return the same result of the controller', async () => {
+    const { sut } = makeSut();
+
+    const httpRequest: HttpRequest = {
+      body: {
+        email: 'any@email.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Tiago',
+      },
+    });
   });
 });
