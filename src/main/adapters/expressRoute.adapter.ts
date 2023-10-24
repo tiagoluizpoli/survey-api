2ab3bd8c4b0d8e2b69ac3af2d524f19bfe40af05
Tiago Poli <tiagoluizpoli@gmail.com>
2023-10-24 20:03:58 -0300

    feat: ensure ExpressRoutesAdapter return error information in a better way

--- src/main/adapters/expressRoute.adapter.ts
+++ src/main/adapters/expressRoute.adapter.ts
@@ -8,7 +8,16 @@ export const adaptRoute = (controller: Controller) => {
     };
 
     const httpResponse = await controller.handle(httpRequest);
-
-    res.status(httpResponse.statusCode).json(httpResponse.body);
+    if (httpResponse.statusCode === 200) {
+      res.status(httpResponse.statusCode).json(httpResponse.body);
+      return;
+    }
+    res.status(httpResponse.statusCode).json({
+      error: {
+        code: httpResponse.body.name,
+        message: httpResponse.body.message,
+        timestamp: new Date().toLocaleString(),
+      },
+    });
   };
 };
