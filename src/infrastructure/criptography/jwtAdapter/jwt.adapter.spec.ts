f1ff9e34f57f9c2361dfc15ad8dea10f459038ff
Tiago Poli <tiagoluizpoli@gmail.com>
2023-10-30 01:20:46 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infrastructure/criptography/jwtAdapter/jwt.adapter.spec.ts
+++ src/infrastructure/criptography/jwtAdapter/jwt.adapter.spec.ts
@@ -1,17 +1,36 @@
 import jwt from 'jsonwebtoken';
 import { JwtAdapter } from './jwt.adapter';
 
+jest.mock('jsonwebtoken', () => ({
+  sign: async (): Promise<string> => {
+    return Promise.resolve('any_token');
+  },
+}));
+
 describe('Jwt Adapter', () => {
   it('should call sign with correct values', async () => {
     const sut = new JwtAdapter('secret');
     const signSpy = jest.spyOn(jwt, 'sign');
     await sut.encrypt('any_id');
 
-    expect(signSpy).toBeCalledWith(
-      {
-        id: 'any_id',
-      },
-      'secret',
-    );
+    expect(signSpy).toBeCalledWith({ id: 'any_id' }, 'secret');
+  });
+
+  it('should return a token on sign success', async () => {
+    const sut = new JwtAdapter('secret');
+
+    const accessToken = await sut.encrypt('any_id');
+
+    expect(accessToken).toBe('any_token');
+  });
+
+  it('should throw if sign throws', async () => {
+    const sut = new JwtAdapter('secret');
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const promise = sut.encrypt('any_id');
+
+    await expect(promise).rejects.toThrow();
   });
 });
