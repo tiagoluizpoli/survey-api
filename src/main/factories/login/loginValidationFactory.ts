c896fb6f7266d88d24f39afcb869b90229a865dc
Tiago Poli <tiagoluizpoli@gmail.com>
2023-10-31 01:34:43 -0300

    feat: add LoginRoute

--- src/main/factories/login/loginValidationFactory.ts
+++ src/main/factories/login/loginValidationFactory.ts
@@ -8,7 +8,7 @@ import { EmailValidatorAdapter } from '../../adapters';
 
 export const makeLoginValidation = (): Validation => {
   const validations: Validation[] = [];
-  const requiredFields = ['name', 'email'];
+  const requiredFields = ['email', 'password'];
   for (const field of requiredFields) {
     validations.push(new RequiredFieldValidation(field));
   }
--- src/main/routes/authentication.routes.ts
+++ src/main/routes/authentication.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import { makeSignUpController } from '../factories';
+import { makeLoginController, makeSignUpController } from '../factories';
 import { adaptRoute } from '../adapters/express/expressRoute.adapter';
 
 export default (router: Router): void => {
-  const controller = makeSignUpController();
-  router.post('/signup', adaptRoute(controller));
+  router.post('/signup', adaptRoute(makeSignUpController()));
+  router.post('/login', adaptRoute(makeLoginController()));
 };
