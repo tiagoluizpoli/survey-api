379ca260d35447df121f94e396acffa15e3977a8
Tiago Poli <tiagoluizpoli@gmail.com>
2023-10-31 01:09:47 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export const env = {
   mongoUrl: process.env.MONGO_URL || 'mongodb://root:example@127.0.0.1:27017/clean-node-api',
   port: process.env.port || 5050,
+  jwtSecret: process.env.JWT_SECRET || '2932ede208b3469e828b95dd7a198bfe',
 };
