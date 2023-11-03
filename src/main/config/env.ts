ec8e01c5ae7a3571413089cf4354527bdb3ddece
Tiago Poli <tiagoluizpoli@gmail.com>
2023-11-02 21:03:41 -0300

    chore: add and configure dotenv

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,5 @@
+import { config } from 'dotenv';
+config();
 export const env = {
   mongoUrl: process.env.MONGO_URL || 'mongodb://root:example@127.0.0.1:27017/clean-node-api',
   port: process.env.port || 5050,
