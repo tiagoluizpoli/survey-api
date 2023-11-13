d46b3e0abc97923720fded2d388283b678a358dc
Tiago Poli <tiagoluizpoli@gmail.com>
2023-11-12 22:03:26 -0300

    test: ensure SignupController returns 500 if authentication throws

--- src/presentation/controllers/signup/signupController.spec.ts
+++ src/presentation/controllers/signup/signupController.spec.ts
@@ -160,4 +160,16 @@ describe('SignUp Controller', () => {
       password: httpRequest.body.password,
     });
   });
+
+  it('should return 500 if Authentication throws', async () => {
+    const { sut, authenticationStub } = makeSut();
+
+    jest.spyOn(authenticationStub, 'authenticate').mockReturnValueOnce(Promise.reject(new Error()));
+
+    const { httpRequest } = makeFakeSignupData();
+
+    const httpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse).toEqual(serverError(new Error()));
+  });
 });
