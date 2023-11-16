b8590657aed1ca8bf59673f057c6ab0da37b80cd
Tiago Poli <tiagoluizpoli@gmail.com>
2023-11-15 21:53:57 -0300

    feat: ensure AddSurveyController returns 500 if AddSurveyUseCase throws

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,5 +1,5 @@
 import { AddSurvey } from '../../../../domain';
-import { badRequest } from '../../../helpers';
+import { badRequest, serverError } from '../../../helpers';
 import { Controller, HttpRequest, HttpResponse, Validation } from '../../../protocols';
 
 export class AddSurveyController implements Controller {
@@ -10,21 +10,25 @@ export class AddSurveyController implements Controller {
   handle = async (httpRequest: HttpRequest): Promise<HttpResponse> => {
     httpRequest;
 
-    const error = this.validation.validate(httpRequest.body);
-    if (error) {
-      return Promise.resolve(badRequest(error));
+    try {
+      const error = this.validation.validate(httpRequest.body);
+      if (error) {
+        return Promise.resolve(badRequest(error));
+      }
+      const { question, answers } = httpRequest.body;
+      await this.addSurvey.add({
+        question,
+        answers,
+      });
+
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: httpRequest.body,
+      };
+
+      return httpResponse;
+    } catch (error) {
+      return serverError(error as Error);
     }
-    const { question, answers } = httpRequest.body;
-    await this.addSurvey.add({
-      question,
-      answers,
-    });
-
-    const httpResponse: HttpResponse = {
-      statusCode: 200,
-      body: httpRequest.body,
-    };
-
-    return httpResponse;
   };
 }
