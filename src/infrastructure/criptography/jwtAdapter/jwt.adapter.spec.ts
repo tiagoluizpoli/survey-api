0599deb0d32d095989928ca32f241159d9aeefe9
Tiago Poli <tiagoluizpoli@gmail.com>
2023-11-23 23:47:26 -0300

    test: ensure jwtAdapter throw if verify throws

--- src/infrastructure/criptography/jwtAdapter/jwt.adapter.spec.ts
+++ src/infrastructure/criptography/jwtAdapter/jwt.adapter.spec.ts
@@ -68,5 +68,15 @@ describe('Jwt Adapter', () => {
       // Assert
       expect(value).toBe('any_value');
     });
+
+    it('should throw if verify throws', async () => {
+      const { sut } = makeSut();
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+        throw new Error();
+      });
+      const promise = sut.decrypt('any_id');
+
+      await expect(promise).rejects.toThrow();
+    });
   });
 });
