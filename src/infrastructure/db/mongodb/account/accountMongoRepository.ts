20ef73edf192455d68c8931cca76d1ad5eab9535
Tiago Poli <tiagoluizpoli@gmail.com>
2023-11-24 00:21:25 -0300

    ensure accountMongoRepository find an account by token without role

--- src/infrastructure/db/mongodb/account/accountMongoRepository.ts
+++ src/infrastructure/db/mongodb/account/accountMongoRepository.ts
@@ -2,13 +2,18 @@ import { ObjectId, WithId } from 'mongodb';
 import {
   AddAccountRepository,
   LoadAccountByEmailRepository,
+  LoadAccountByTokenRepository,
   UpdateAccessTokenRepository,
 } from '../../../../data';
 import { AddAccountModel, AccountModel } from '../../../../domain';
 import { MongoHelper } from '../helpers';
 
 export class AccountMongoRepository
-  implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository
+  implements
+    AddAccountRepository,
+    LoadAccountByEmailRepository,
+    UpdateAccessTokenRepository,
+    LoadAccountByTokenRepository
 {
   constructor() {}
 
@@ -24,6 +29,20 @@ export class AccountMongoRepository
   loadByEmail = async (email: string): Promise<AccountModel | null> => {
     const accountCollection = await MongoHelper.getCollection('accounts');
     const account = await accountCollection.findOne<WithId<AccountModel>>({ email });
+
+    if (account === null) {
+      return null;
+    }
+    return MongoHelper.map(account);
+  };
+
+  loadByToken = async (token: string, role?: string): Promise<AccountModel | null> => {
+    role;
+    const accountCollection = await MongoHelper.getCollection('accounts');
+    const account = await accountCollection.findOne<WithId<AccountModel>>({
+      accessToken: token,
+      role,
+    });
 
     if (account === null) {
       return null;
