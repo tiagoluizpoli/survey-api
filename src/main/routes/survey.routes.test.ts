bdb00b201b74c2f1bdf910078b9a2851f512c83b
Tiago Poli <tiagoluizpoli@gmail.com>
2023-11-24 00:52:00 -0300

    test: ensure addSurveyRoute returns 403 if no accessToken is provided

--- src/main/routes/survey.routes.test.ts
+++ src/main/routes/survey.routes.test.ts
@@ -19,7 +19,7 @@ describe('Survey Routes', () => {
   });
 
   describe('POST /surveys', () => {
-    it('should return 204 on success', async () => {
+    it('should return 403 on add survey without accessToken', async () => {
       await request(app)
         .post('/api/surveys')
         .send({
@@ -34,7 +34,7 @@ describe('Survey Routes', () => {
             },
           ],
         })
-        .expect(204);
+        .expect(403);
     });
   });
 });
