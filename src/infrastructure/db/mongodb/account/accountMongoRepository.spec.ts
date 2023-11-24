995533daa1077457ba151d65f9d97b25559f7be9
Tiago Poli <tiagoluizpoli@gmail.com>
2023-11-24 01:52:03 -0300

    test: ensure AccountMongoRepository allows admin to access any route

--- src/infrastructure/db/mongodb/account/accountMongoRepository.spec.ts
+++ src/infrastructure/db/mongodb/account/accountMongoRepository.spec.ts
@@ -117,17 +117,50 @@ describe('AccountRepository (Mongodb)', () => {
       expect(account?.password).toBe('any_password');
     });
 
-    it('should return an account on loadByToken with role', async () => {
+    it('should return an account on loadByToken with admin role', async () => {
       const { sut } = makeSut();
       const { addAccount } = makeFakeData();
       await accountCollection.insertOne({
         ...addAccount,
         id: 'any_id',
         accessToken: 'any_token',
-        role: 'any_role',
+        role: 'admin',
       });
 
-      const account = await sut.loadByToken('any_token', 'any_role');
+      const account = await sut.loadByToken('any_token', 'admin');
+
+      expect(account).toBeTruthy();
+      expect(account?.id).toBeTruthy();
+      expect(account?.name).toBe('any_name');
+      expect(account?.email).toBe('any@email.com');
+      expect(account?.password).toBe('any_password');
+    });
+
+    it('should return null on loadByToken with invalid role', async () => {
+      const { sut } = makeSut();
+      const { addAccount } = makeFakeData();
+      await accountCollection.insertOne({
+        ...addAccount,
+        id: 'any_id',
+        accessToken: 'any_token',
+      });
+
+      const account = await sut.loadByToken('any_token', 'admin');
+
+      expect(account).toBeFalsy();
+    });
+
+    it('should return an account on loadByToken if user is admin', async () => {
+      const { sut } = makeSut();
+      const { addAccount } = makeFakeData();
+      await accountCollection.insertOne({
+        ...addAccount,
+        id: 'any_id',
+        accessToken: 'any_token',
+        role: 'admin',
+      });
+
+      const account = await sut.loadByToken('any_token');
 
       expect(account).toBeTruthy();
       expect(account?.id).toBeTruthy();
