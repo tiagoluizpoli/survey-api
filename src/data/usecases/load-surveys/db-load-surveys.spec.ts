5a8dc039e0ae84f1ce1bd72aa0ff9e6a5daba5ae
Tiago Poli <tiagoluizpoli@gmail.com>
2023-11-26 17:28:01 -0300

    test ensure DbLoadSurveys throw if LoadSurveyRepository throws

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -77,4 +77,19 @@ describe('DbLoadSurveys', () => {
     // Assert
     expect(surveysResult).toEqual(surveys);
   });
+
+  it('shoud throw if LoadSurveyRepository throws', async () => {
+    // Arrange
+    const { sut, loadSurveysRepositoryStub } = makeSut();
+
+    jest
+      .spyOn(loadSurveysRepositoryStub, 'loadAll')
+      .mockReturnValueOnce(Promise.reject(new Error()));
+
+    // Act
+    const promise = sut.load();
+
+    // Assert
+    await expect(promise).rejects.toThrow();
+  });
 });
