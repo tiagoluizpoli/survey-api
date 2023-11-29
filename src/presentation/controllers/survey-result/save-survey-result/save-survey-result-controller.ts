74eb3562e8c20aae0d1da05cf8af83382389ff3c
Tiago Poli <tiagoluizpoli@gmail.com>
2023-11-29 01:55:47 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,14 +1,18 @@
-import { LoadSurveyById } from '@/domain';
+import { LoadSurveyById, SaveSurveyResult } from '@/domain';
 import { InvalidParamError } from '@/presentation/errors';
 import { forbidden, ok, serverError } from '@/presentation/helpers';
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols';
 
 export class SaveSurveyResultController implements Controller {
-  constructor(private readonly loadSurveyById: LoadSurveyById) {}
+  constructor(
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly saveSurveyResult: SaveSurveyResult,
+  ) {}
   handle = async (httpRequest: HttpRequest): Promise<HttpResponse> => {
     try {
       const { surveyId } = httpRequest.params;
       const { answer } = httpRequest.body;
+      const { accountId } = httpRequest;
 
       const survey = await this.loadSurveyById.loadById(surveyId);
       if (!survey) {
@@ -19,6 +23,13 @@ export class SaveSurveyResultController implements Controller {
       if (!answers.includes(answer)) {
         return forbidden(new InvalidParamError('answer'));
       }
+
+      await this.saveSurveyResult.save({
+        accountId: accountId || '',
+        surveyId,
+        answer,
+        date: new Date(),
+      });
 
       return ok({});
     } catch (error) {
